cceba4e7bfad7dc3f97dcb4028935298df7696b2
Jade 648 <maddiepeterson375@gmail.com>
2023-02-06 08:51:21 -0600

    Updated Balloons. js file

--- js/balloons.js
+++ js/balloons.js
@@ -1,3 +1,14 @@
 $(function(){
   $('#birthday').pickadate({ format: 'mmmm, d' });
 });
+$('.form-check-input').each(function () {
+  $(this).prop('checked', false);
+});
+$('.form-check-input').on('change', function () {
+  // make the image visible
+  $('#' + this.id + 'Img').css('visibility', 'visible')
+  // animate balloon in/out based on checkbox
+  $(this).is(':checked') ?
+   $('#' + this.id + 'Img').removeClass().addClass('animated bounceInDown') :
+   $('#' + this.id + 'Img').addClass('animated bounceOutUp');
+});
