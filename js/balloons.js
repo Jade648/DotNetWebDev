354cbe6677fef6a8caf1507d1b31907afe949040
Jade 648 <maddiepeterson375@gmail.com>
2023-02-11 09:43:11 -0600

    Updated Css , html and js files

--- js/balloons.js
+++ js/balloons.js
@@ -13,14 +13,35 @@ $('.form-check-input').on('change', function () {
    $('#' + this.id + 'Img').addClass('animate__animated animate__bounceOutUp');
 });
 
-function Checkall(){
-  var items= document.getElementsByName('btn-group');
-  {
-    if(items[i].type=='checkbox')
-    items[i].checked=true;
-  }
+function RandomizeAnimations(){
+  let randomValue = animations(Math.floor(Math.random()*animations.length));
+  var animations=[
+    
+    "animate_animated animate_bounce",
+    "animate_animated animate_wobble",
+    "animate_animated animate_flash",
+    "animate_animate animate_swing",
+    "animate_animated animate_heartbeat",
+    "animate_animated animate_pulse",
+    "animate_animated animate_tada"
+
+  ];
+
+  var randomOrder = animations.randomize();
+  alert("This will be the order:/n-"+randomOrder)
+
 }
+
+ function uncheckAll(){
+
+  document.querySelectorAll('input [type ="checkbox"]')
+  .forEach(el => el.checked = false);
+
+ }
+ document.querySelector('button').addEventListener('click',uncheckAll)
+
 $('.submit').on('click', function(e) {
+  if("no balloons checked"){
   e.preventDefault();
   // first pause the audio (in case it is still playing)
   toast.show();
@@ -29,4 +50,5 @@ $('.submit').on('click', function(e) {
   // play audio
   toast.Hide();
   $('#toast').toast({ autohide: false }).toast('show');   
+  }
 });
